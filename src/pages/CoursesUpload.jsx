d117422801c911c88b484c6b686fcba3f3cd3845
Philipp Mulfinger <info@phil-splash.de>
2024-02-03 13:14:26 +0100

    update env

--- src/pages/CoursesUpload.jsx
+++ src/pages/CoursesUpload.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import FormDataInput from "../components/FormDataInput.jsx";
-/* import { useLocation } from "react-router-dom"; */
 
 function CoursesUpload() {
   const [successMessage, setSuccessMessage] = useState("");
