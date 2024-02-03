32bea13a877d51f793da3611a9752e4f175cf377
Philipp Mulfinger <info@phil-splash.de>
2024-02-03 12:51:22 +0100

    ready for vercel deploy

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={"/ai_news/"}>
+  <BrowserRouter>
     <AuthProvider>
       {/* <React.StrictMode> */}
       <App />
